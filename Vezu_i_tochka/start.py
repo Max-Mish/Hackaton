136aa55713d45701f93fc1965b81ef215e91d9b1
Max_Mish <max_mish@inbox.ru>
2022-10-16 20:34:17 +0300

    Выполнено скачивание и выгрузка данных

--- Vezu_i_tochka/start.py
+++ Vezu_i_tochka/start.py
@@ -1,3 +1,12 @@
+from create_connection import create_connection
 from download_files import download_files
+from upload_files import *
 
-download_files('c:/Users/maxim/Downloads/Hackaton/Files/')
+connection = create_connection(
+    'Vezu_i_tochka', 'postgres', 'admin', 'localhost', '5432'
+)
+path = 'c:/Users/maxim/Downloads/Hackaton/Files/'
+download_files(path)
+upload_payments(connection, path)
+upload_waybills(connection, path)
+connection.close()
