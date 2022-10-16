4899513f7f3c4347704ec3e0ef6323dc1c332506
Max_Mish <max_mish@inbox.ru>
2022-10-16 13:00:10 +0300

    Функция подключения к базе данных выделена в отдельный модуль

--- Vezu_i_tochka/import_db.py
+++ Vezu_i_tochka/import_db.py
@@ -1,26 +1,9 @@
-import psycopg2
-
-
-def create_connection(db_name, db_user, db_password, db_host, db_port):
-    bd_connection_result = None
-    try:
-        bd_connection_result = psycopg2.connect(
-            database=db_name,
-            user=db_user,
-            password=db_password,
-            host=db_host,
-            port=db_port,
-        )
-        print('Connection to PostgreSQL DB successful')
-    except psycopg2.OperationalError as e:
-        print(f"The error '{e}' occurred")
-    return bd_connection_result
+from create_connection import create_connection
 
 
 def show_tables(bd_connection):
     try:
         with bd_connection.cursor() as cursor:
-            bd_tables = []
             cursor.execute("""SELECT table_name FROM information_schema.tables
                    WHERE table_schema = 'main'""")
             return list(cursor.fetchall())
